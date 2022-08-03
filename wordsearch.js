77de9b6af477affd15c959c3fd82cf4c8c8e4494
vagrant <bahar.hosseini@gmail.com>
2022-08-03 18:06:30 +0000

    initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//pair programming (Bahar & Fatima)
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  let result = [];
+  for (let i = 0; i < letters[0].length; i ++) {
+    result.push([]);
+  }
+
+  for (let row = 0; row < letters.length; row++) {
+    for (let col = 0; col < letters[row].length; col++) {
+      result[col][row] = letters[row][col];
+    }
+  }
+
+  const verticalJoin = letters.map(l => l.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+  
+};
+
+module.exports = wordSearch;
